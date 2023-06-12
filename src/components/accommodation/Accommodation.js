ff9c19abfa7b1a56de353d868d22916f02145562
Alexandra Sicoe <sicoealexandra97@gmail.com>
2023-06-12 16:58:48 +0300

    finished accommodation section

--- src/components/accommodation/Accommodation.js
+++ src/components/accommodation/Accommodation.js
@@ -1,15 +1,47 @@
 import style from "./Accommodation.style";
 const Accommodation = () => {
   return (
-    <div className="container-fluid">
-      <div className="row  justify-content-center">
+    <div className="container-fluid mb-5">
+      <div className="row  justify-content-center ">
         <div className="col-12 d-flex justify-content-center ">
           <p style={style.paragraph}>rooms & suits</p>
         </div>
       </div>
       <div className="row  justify-content-center">
-        <div className="col-12 d-flex justify-content-center text-center">
-          <h2 style={style.title}>Stay in guest rooms touched by history</h2>
+        <div className="col-12 mx-5 d-flex justify-content-center text-center">
+          <h2 className="fs-1" style={style.title}>
+            Stay in guest rooms touched by history
+          </h2>
+        </div>
+        <div className="row justify-content-center">
+          <div
+            className="col-sm-12 text-center px-5 text-lg-start col-lg-6"
+            style={style.accommodationP}
+          >
+            <p>
+              With a total of 120 rooms and suites, El Palace Barcelona is able
+              to offer its guests the newest technology while retaining the
+              classic elegance that has been its trademark since it was born as
+              the former Ritz of Barcelona.
+            </p>
+          </div>
+          <div
+            className="col-sm-12 text-center px-5 text-lg-start col-lg-6"
+            style={style.accommodationP}
+          >
+            <p>
+              Our rooms come in many different styles and sizes. All of them
+              have been carefully furnished for maximum comfort, to ensure that
+              the perfect room awaits you. All that is left is to choose.
+            </p>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-12 text-center">
+            <a href="#" style={{ ...style.learnMore, ...style.borderBottom }}>
+              Learn more
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- src/components/accommodation/Accommodation.style.js
+++ src/components/accommodation/Accommodation.style.js
@@ -9,12 +9,33 @@ const style = {
     marginBottom: "10px",
   },
   title: {
-    fontSize: "70px",
     color: "black",
     letterSpacing: "2.33px",
-    maxWidth: "614px",
+    maxWidth: "450px",
     fontWeight: 400,
     marginBottom: "40px",
+  },
+  accommodationP: {
+    maxWidth: "600px",
+  },
+  accommodationText: {
+    fontSize: "18px",
+    lineHeight: "28px",
+    color: "black",
+  },
+  learnMore: {
+    textTransform: "uppercase",
+    fontSize: "12px",
+    color: "#D8272E",
+    letterSpacing: "2px",
+    fontWeight: 600,
+    marginTop: "45px",
+    marginBottom: "10px",
+    textDecoration: "none",
+  },
+  borderBottom: {
+    borderBottom: "1px solid #D8272E",
+    paddingBottom: "10px",
   },
 };
 export default style;
